5e272bb5dd016c0641e887a266cd2a574f37c2e4
Sed <mahmoudseddiga@gmail.com>
2025-11-03 16:28:28 +0100

    new init

--- init.py
+++ init.py
@@ -1,30 +1,30 @@
 import numpy as np
 
-def normal(shape, mean=0.0, std=0.02):
-    return np.random.normal(mean, std, size=shape)
+def normal(dim_in,dim_out, mean=0.0, std=0.02):
+    return np.random.normal(mean, std, size=(dim_in,dim_out))
 
-def xavier_uniform(shape):
-    in_features, out_features = shape[1], shape[0]
+def xavier_uniform(dim_in,dim_out):
+    in_features, out_features = dim_out, dim_in
     lim = (6.0 / (in_features + out_features)) ** 0.5
-    return np.random.uniform(-lim, lim, size=shape)
+    return np.random.uniform(-lim, lim, size=(dim_in,dim_out))
 
-def xavier_normal(shape):
-    in_features, out_features = shape[1], shape[0]
+def xavier_normal(dim_in,dim_out):
+    in_features, out_features = dim_out, dim_in
     std = (2.0 / (in_features + out_features)) ** 0.5
-    return np.random.normal(0.0, std, size=shape)
+    return np.random.normal(0.0, std, size=(dim_in,dim_out))
 
-def he_normal(shape):
-    in_features = shape[1]
+def he_normal(dim_in,dim_out):
+    in_features = dim_out
     std = (2.0 / in_features) ** 0.5
-    return np.random.normal(0.0, std, size=shape)
+    return np.random.normal(0.0, std, size=(dim_in,dim_out))
 
-def he_uniform(shape):
-    in_features = shape[1]
+def he_uniform(dim_in,dim_out):
+    in_features = dim_out
     lim = (6.0 / in_features) ** 0.5
-    return np.random.uniform(-lim, lim, size=shape)
+    return np.random.uniform(-lim, lim, size=(dim_in,dim_out))
 
-def zeros(shape):
-    return np.zeros(shape)
+def zeros(dim_in,dim_out):
+    return np.zeros(dim_in,dim_out)
 
-def random(shape, low=-0.05, high=0.05):
-    return np.random.uniform(low, high, size=shape)
+def random(dim_in,dim_out, low=-0.05, high=0.05):
+    return np.random.uniform(low, high, size=(dim_in,dim_out))
