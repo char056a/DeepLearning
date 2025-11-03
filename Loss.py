c1d5597c558f743a94abe0e0c81d8f309be24e82
s224483 <s224483@student.dtu.dk>
2025-11-03 16:20:01 +0100

    Added cross entropy loss

--- Loss.py
+++ Loss.py
@@ -12,16 +12,22 @@ def softmax(output):
 
     return (np.exp(output))/np.sum(np.exp(output))
 
-# Cross-entropy 
+# Cross-entropy (only for one training example)
 
+def cross_entropy_single(correct_onehot, network_output):
 
+    S = softmax(network_output)
+    S = np.sum(S * correct_onehot)
+    S = -np.log(S)
+    return S 
 
 # Test
-
+print("MSE:")
 print(MSE(output = np.array([2,3]), target = np.array([4,6])))
-
-print(softmax(np.array([5,5,7])))
-
+print("Softmax:")
+print(softmax(np.array([1,2,20,3,4])))
+print("Cross:")
+print(cross_entropy_single(np.array([0,0,1,0,0]),[1,2,20,3,4]))
 
 
 
