ee0e89156a8944e80a31c03b1a02035de40cbbd7
s224483 <s224483@student.dtu.dk>
2025-11-03 15:58:59 +0100

    Added softmax function

--- Loss.py
+++ Loss.py
@@ -6,15 +6,21 @@ def MSE(output, target):
     
     return np.sum((target-output)**2/len(target))
 
+# Softmax 
+
+def softmax(output):
+
+    return (np.exp(output))/np.sum(np.exp(output))
+
 # Cross-entropy 
-
-
 
 
 
 # Test
 
 print(MSE(output = np.array([2,3]), target = np.array([4,6])))
+
+print(softmax(np.array([5,5,7])))
 
 
 
