4273d6846220f017610adb7a59a298c2f9101a34
s224483 <s224483@student.dtu.dk>
2025-11-10 16:04:22 +0100

    Defined hyperparameters

--- Training_loop.py
+++ Training_loop.py
@@ -5,5 +5,14 @@ wandb.login()
 # Project that the run is recorded to
 project = "..."
 
+# Define hyperparameters 
+config = {
+    "epochs": 10,
+    "learning_rate": 0.01,
+    "hidden_sizes": [2,2],
+    "activation": "Relu",
+    "init": "xavier_uniform"
+}
 
+# Initialize run 
 
