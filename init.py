9e16b3a6e197ff76ffe44029fbf0e0754d023b97
Sed <mahmoudseddiga@gmail.com>
2025-11-26 14:31:45 +0100

    init fixed

--- init.py
+++ init.py
@@ -14,12 +14,12 @@ def xavier_normal(dim_in,dim_out):
     return np.random.normal(0.0, std, size=(dim_out,dim_in))
 
 def he_normal(dim_in,dim_out):
-    in_features = dim_out
+    in_features = dim_in
     std = (2.0 / in_features) ** 0.5
     return np.random.normal(0.0, std, size=(dim_out,dim_in))
 
 def he_uniform(dim_in,dim_out):
-    in_features = dim_out
+    in_features = dim_in
     lim = (6.0 / in_features) ** 0.5
     return np.random.uniform(-lim, lim, size=(dim_out,dim_in))
 
